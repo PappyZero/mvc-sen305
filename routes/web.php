f3f0c593cfba98d2753f219dcd3fa725a2465189
PappyZero <amaddinmajid24@gmail.com>
2023-11-20 11:44:07 +0100

    Class 20-Nov ended

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/register_user', UserController::class)->name('register_user');
+
 
 Auth::routes();
 
